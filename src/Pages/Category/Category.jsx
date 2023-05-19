8eff39bcbd0d4b8728af5242848af851f2c0f74b
yovel-halabia <yovelh247@gmail.com>
2023-05-19 13:00:19 +0300

    add spinner when products loading in category page

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import React from "react";
 import "./Category.css";
+import { TailSpin } from "react-loader-spinner";
 
 import { useParams, useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
@@ -28,7 +29,6 @@ export default function Category({ prevPath }) {
 	const { pathname } = useLocation();
 	const { categoryId } = useParams();
 	const { data } = useGetProductQuery(`get-category-products/${categoryId}`);
-
 	return (
 		<motion.div
 			style={{ width: "100%", position: "absolute", height: "100vh" }}
@@ -44,22 +44,22 @@ export default function Category({ prevPath }) {
 			transition={{ type: "spring", stiffness: 70 }}
 			custom={{ pathName: pathname, prevPath: prevPath }}>
 			<div className="category page">
-				{data && (
-					<>
-						<h1>
-							{categoryId
-								.split("-")
-								.map((name) => {
-									return name[0].toUpperCase() + name.slice(1);
-								})
-								.join(" ")}
-						</h1>
-						<div className="category__items-container">
-							{data.map((item) => (
-								<CategoryItem item={item} />
-							))}
-						</div>
-					</>
+				<h1>
+					{categoryId
+						.split("-")
+						.map((name) => {
+							return name[0].toUpperCase() + name.slice(1);
+						})
+						.join(" ")}
+				</h1>
+				{data ? (
+					<div className="category__items-container">
+						{data?.map((item) => (
+							<CategoryItem item={item} />
+						))}
+					</div>
+				) : (
+					<TailSpin color="#614FE0" width="150" height="150" wrapperClass="category__spinner" />
 				)}
 			</div>
 		</motion.div>
